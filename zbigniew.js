25808f7691d6dddf9c1e160f32f809c09541e732
Dawid <39349444+Indexerrowaty@users.noreply.github.com>
2020-10-17 01:53:55 +0200

    Update zbigniew.js

--- zbigniew.js
+++ zbigniew.js
@@ -12,8 +12,9 @@ import token from "./dane/config.js";
 import cytaty from "./dane/cytaty.js";
 
 zbysiu.on("message", e => {
-    if ((e.author.bot || e.mentions.everyone) && !e.mentions.has(zbysiu.user)) return;
-    e.channel.send(cytaty[Math.floor(Math.random() * cytaty.length)]);
+    if (!(e.author.bot || e.mentions.everyone) && e.mentions.has(sad.user)) {
+        e.channel.send(cytaty[Math.floor(Math.random() * cytaty.length)]);
+    }
 });
 
 zbysiu.login(token);
